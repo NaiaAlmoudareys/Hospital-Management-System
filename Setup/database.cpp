09f348691d590649a283d1ec8dbd262a8454c730
NaiaAlmoudareys <151873469+NaiaAlmoudareys@users.noreply.github.com>
2024-07-22 00:00:19 -0400

    Create database.cpp

---
+++ Setup/database.cpp
@@ -0,0 +1,29 @@
+#include "database.h"
+#include <iostream>
+
+Database::Database(const std::string& dbName) {
+    if (sqlite3_open(dbName.c_str(), &db) != SQLITE_OK) {
+        std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+        db = nullptr;
+    }
+}
+
+Database::~Database() {
+    if (db) {
+        sqlite3_close(db);
+    }
+}
+
+bool Database::executeQuery(const std::string& query) {
+    char* errMsg = nullptr;
+    if (sqlite3_exec(db, query.c_str(), nullptr, nullptr, &errMsg) != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+        return false;
+    }
+    return true;
+}
+
+sqlite3* Database::getDB() const {
+    return db;
+}
